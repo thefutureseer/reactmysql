2c8536b469fbcd27301f13ebdc07e4e102d1d195
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-06-28 16:20:58 -0700

    react add product

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+ state = {
+   products: [],
+   product: {
+     name: 'sample',
+     price: 10
+   }
+ };
+
+ componentDidMount() {
+  this.getProducts();
+ }
+
+ getProducts = _ => {
+   fetch(`http://localHost:4000/products`)
+   .then(response => response.json())
+   .then(response => this.setState({products: response.data}))
+   .catch(err => console.error(err) )
+ }
+
+ addProduct = _ => {
+  const { product } = this.state;
+  fetch(`http://localhost:4000/products/add?name=${product.name}&price=${product.price}`)
+    .then(this.getProducts)
+    .catch(err => console.error(err))
+ }
+
+ renderProduct = ({ product_id, name }) => <div key={ product_id }>{name}</div>
+
+ render() {
+  const { products, product  }= this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map(this.renderProduct)}  
+      <div>
+       <input 
+         value= {product.name} 
+         onChange={e => this.setState({ product:{...product, name: e.target.value} }) }/>
+       <input 
+         value= {product.price}
+         onChange={e => this.setState({ product:{...product, price: e.target.value} }) }/> 
+       <button onClick={this.addProduct}>Add product</button>
+      </div>
+    
+
     </div>
   );
+ }
 }
 
 export default App;
